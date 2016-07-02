041c994fa4ddd199474305aea4b726bc5afa82eb
Sasha Nikiforov <alex.nikiforov@rf-lab.org>
2016-07-01 20:18:40 -0700

    DNN: Layer: cleanup

--- xor-dnn.py
+++ xor-dnn.py
@@ -107,84 +107,20 @@ class NeuronLayer(object):
         raise NotImplementedError, "NeuronLayer class, backward() has to be implemented"
 
 class OutputLayer(NeuronLayer):
-    def __init__(self):
-        raise NotImplementedError, "OutputLayer class, not implemeted"
+    def backward(self, desired_output):
+        pass
 
 class HiddenLayer(NeuronLayer):
-    _isOutput = False
-
-    def __init__(self, number_of_neurons, number_of_inputs, number_of_neurons_next_layer, isOutput = False):
-        self._isOutput = isOutput
+    def backward(self, local_gradient):
+        pass
 
 
 input_vector = np.array((1, 2)).reshape(2, 1)
 
-nl_1 = NeuronLayer(number_of_neurons = 2, number_of_inputs = 2)
+nl_1 = HiddenLayer(number_of_neurons = 2, number_of_inputs = 2)
 nl_1.forward(input_vector)
 print("Neural layer 1:" + str(nl_1))
 
-nl_2 = NeuronLayer(number_of_neurons = 2, number_of_inputs = 2)
+nl_2 = OutputLayer(number_of_neurons = 2, number_of_inputs = 2)
 nl_2.forward(nl_1.get_layer_output())
 print("Neural layer 2:" + str(nl_2))
-
-
-# print( "output:\n" + str(nl.get_layer_output()) )
-
-# HiddenLayer(number_of_neurons = 3, number_of_inputs = 2, number_of_neurons_next_layer = 1)
-
-# # input vector
-# x = ((1, 0, 0), (1, 0, 1), (1, 1, 0), (1, 1, 1))
-#
-# # desired vector
-# y = (1, 0, 0, 1)
-
-# # first layer x -> w1
-# w1_x = np.array(([1], [1], [1]))
-# w2_x = np.array(([1], [1], [1]))
-# print("==>" + str(w2_x))
-
-# # second layer
-# w_w12 = np.array(([1], [1], [1]))
-#
-# iteration = 0
-# # Forward propagation x -> o
-# # first layer computation
-# v1_1 = np.array(x[iteration]).dot(w1_x)
-# v2_1 = np.array(x[iteration]).dot(w2_x)
-# print ("1: v1_1:%s v2_1:%s" % (str(v1_1), str(v2_1)))
-#
-# a = 1.7159
-# b = 0.6666
-# phi_v1_1 = a * np.tanh(b*v1_1)
-# phi_v2_1 = a * np.tanh(b*v2_1)
-# print ("1: phi_v1:%s phi_v2:%s" % (str(phi_v1_1), str(phi_v2_1)))
-#
-# y1_1 = np.array([1, phi_v1_1, phi_v2_1])
-# print ("1: y1_1:%s" % str(y1_1))
-#
-# # second layer
-# v1_2 = y1_1.dot(w_w12)
-# print ("2: v1_2:%s" % str(v1_2))
-#
-# phi_v1_2 = a * np.tanh(b * v1_2)
-# print ("2: phi_v1_2:%s" % str(phi_v1_2))
-#
-# # calculate error
-# e = y[iteration] - phi_v1_2
-# print ("e:%s" % str(e))
-#
-# # Back propagation x <- e
-# delta = e * b/a * (a - phi_v1_2) * (a + phi_v1_2)
-# print ("delta:%s" % str(delta))
-#
-# w_w3_2 =  w_w12 + 0.5 * delta * y1_1.reshape(3, 1)
-# print ("w_w3_2:%s" % str(w_w3_2))
-#
-# delta_0_2 = b/a * (a - 1) * (a + 1) * delta * w_w12[0]
-# delta_1_2 = b/a * (a - phi_v1_1) * (a + phi_v1_1) * delta * w_w12[1]
-# delta_2_2 = b/a * (a - phi_v2_1) * (a + phi_v2_1) * delta * w_w12[2]
-#
-# print ("delta_0_2:%s delta_1_2:%s delta_2_2:%s" % (str(delta_0_2), str(delta_1_2), str(delta_2_2)))
-#
-# w_w1_1 =  w1_x + 0.5 * delta * x[iteration].reshape(3, 1)
-# print ("w_w1_1:%s" % str(w_w1_1))
