02f07b438f583f616ac700f36e0dbce3c6ae4020
Sasha Nikiforov <alex.nikiforov@rf-lab.org>
2016-07-15 03:53:09 -0700

    OneLayer: HardLim: seems done, need to finish code to incorporate MNIST test

--- one_layer_perceptron.py
+++ one_layer_perceptron.py
@@ -5,14 +5,19 @@ from lib.transferfunction import HardLim
 from lib.neuron import NeuronLayer
 
 layer = NeuronLayer(
-    num_of_neurons = 1,
-    num_of_inputs = 1,
+    num_of_neurons = 2,
+    num_of_inputs = 3,
     hasBias=False,
     activation_function = HardLim)
 
 layer.calculate_local_field(np.array(([1, -1, -1])).reshape(3, 1))
 print("Layer: " + str(layer))
-layer.trainining(np.array(([0, 1])).reshape(2, 1))
+layer.trainining(np.array(([-1, -1])).reshape(2, 1))
+# print("Layer: " + str(layer))
+
+layer.calculate_local_field(np.array(([1, -1, -1])).reshape(3, 1))
+# print("Layer: " + str(layer))
+layer.trainining(np.array(([-1, -1])).reshape(2, 1))
 print("Layer: " + str(layer))
 
 # layer.calculate_local_field(np.array(([1, 1, -1])))
