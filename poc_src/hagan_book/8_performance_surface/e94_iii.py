853e28a7913fd9e655c3adc08034fab38b3ae6a2
Sasha Nikiforov <nikiforov.al@gmail.com>
2016-10-03 04:14:11 -0700

    neural network design: solution for the e9.4 iii and iv

--- poc_src/hagan_book/8_performance_surface/e94_iii.py
+++ poc_src/hagan_book/8_performance_surface/e94_iii.py
@@ -1,42 +1,41 @@
 #!/bin/env python
 
 # Sasha Nikiforov
-# [NND] excersize E9.2 i
+# [NND] excersize E9.4 iii
 
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 # i - sketch a contour plot
-ylist = np.linspace(-3.0, 3.0)
-xlist = np.linspace(-3.0, 3.0)
+ylist = np.linspace(-1, 1)
+xlist = np.linspace(-1, 1)
 
 X, Y = np.meshgrid(xlist, ylist)
-Z = 3.5 * X*X - 6*X*Y - Y*Y
+Z = 9/2*X*X - 2*X*Y + 3*Y*Y + 2*X - Y
 
 fig = plt.figure()
 ax = fig.add_subplot(121, projection='3d')
 ax.plot_wireframe(X, Y, Z)
 
 cc = fig.add_subplot(122)
-cc.contour(X, Y, Z)
+cc.contour(X, Y, Z, 10)
 
 from sympy import Function, hessian, pprint
 from sympy.abc import x, y 
 
-z = 3.5 * x*x - 6*x*y - y*y
+z = 4.5*x*x - 2*x*y + 3*y*y + 2*x - y
 pprint(hessian(z, (x, y)))
 pprint(hessian(z, (x, y)).eigenvals())
 
-# eigenvals are -5 and 10, so alpha < 2/10 < 0.2
-alpha = 0.1
-iterations = 2
-solution = np.zeros((iterations, 2))
+# eigenvals are -5 and 10, so alpha < 2/10 = 0.2
+alpha = 0.1 
+solution = np.zeros((10, 2))
 x0 = np.array((1, 1)).reshape(2, 1)
 solution[0, :] = x0.reshape(1, 2)
 print("x0: " + str(solution[0, :]))
-for i in range(0, iterations):
-    g = np.array((7, -6, -6, -2)).reshape(2, 2).dot(x0)
+for i in range(1, 10):
+    g = np.array((9, -2, -2, 6)).reshape(2, 2).dot(x0) + np.array((2, -1)).reshape(2, 1)
     x = x0 - alpha * g
 
     x0 = x
@@ -44,7 +43,6 @@ for i in range(0, iterations):
     print("x: " + str(solution[i, :]))
 
 cc.plot(solution[:, 0], solution[:, 1], 'r^')
-
 
 plt.grid()
 plt.show()
--- poc_src/hagan_book/8_performance_surface/e94_iv.py
+++ poc_src/hagan_book/8_performance_surface/e94_iv.py
@@ -1,42 +1,42 @@
 #!/bin/env python
 
 # Sasha Nikiforov
-# [NND] excersize E9.2 i
+# [NND] excersize E9.4 iv
 
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 # i - sketch a contour plot
-ylist = np.linspace(-3.0, 3.0)
-xlist = np.linspace(-3.0, 3.0)
+ylist = np.linspace(-3, 3)
+xlist = np.linspace(-3, 3)
 
 X, Y = np.meshgrid(xlist, ylist)
-Z = 3.5 * X*X - 6*X*Y - Y*Y
+Z = -3.5*X*X - 6*X*Y + Y*Y
 
 fig = plt.figure()
 ax = fig.add_subplot(121, projection='3d')
 ax.plot_wireframe(X, Y, Z)
 
 cc = fig.add_subplot(122)
-cc.contour(X, Y, Z)
+cc.contour(X, Y, Z, 10)
 
 from sympy import Function, hessian, pprint
 from sympy.abc import x, y 
 
-z = 3.5 * x*x - 6*x*y - y*y
+z = -3.5*x*x - 6*x*y + y*y
 pprint(hessian(z, (x, y)))
 pprint(hessian(z, (x, y)).eigenvals())
 
-# eigenvals are -5 and 10, so alpha < 2/10 < 0.2
-alpha = 0.1
+# eigenvals are 4 and 16, so alpha < 2/16 < 0.25
 iterations = 2
+alpha = 0.10
 solution = np.zeros((iterations, 2))
 x0 = np.array((1, 1)).reshape(2, 1)
 solution[0, :] = x0.reshape(1, 2)
 print("x0: " + str(solution[0, :]))
-for i in range(0, iterations):
-    g = np.array((7, -6, -6, -2)).reshape(2, 2).dot(x0)
+for i in range(1, iterations):
+    g = np.array((-7, -6, -6, 2)).reshape(2, 2).dot(x0)
     x = x0 - alpha * g
 
     x0 = x
@@ -44,7 +44,6 @@ for i in range(0, iterations):
     print("x: " + str(solution[i, :]))
 
 cc.plot(solution[:, 0], solution[:, 1], 'r^')
-
 
 plt.grid()
 plt.show()
